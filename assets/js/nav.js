a1392499dcc3bb3d3849ca609d2f48b5240edc0a
FranckL <perez_salazarf@hotmail.com>
2022-01-17 02:46:43 -0600

    mejora barr y nav-sub-nav y con js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,19 +1,33 @@
+// let navShow= document.querySelector('.nav__box-show');
+
 const barrBox= document.getElementById('barr__box');
-let barrNav= document.getElementById('barr__nav');
-let iconMenu= document.getElementById('iconMenu');
-let navBox= document.getElementById('nav-box');
-// let navShow= document.querySelector('.barr__nav-show');
 
 function data(elem){
+    let barrNav= document.getElementById('nav__box');
+    let iconMenu= document.getElementById('iconMenu');
+    let navBox= document.getElementById('nav__box-btn');
+    let linkSubNav= document.querySelectorAll('.nav__linkBox');
+    
+    let navSubBox= document.querySelectorAll('.box__subMenu');
     if(elem == iconMenu){
-        barrNav.classList.toggle('barr__nav-show')
+        barrNav.classList.toggle('nav__box-show');
     }else if(elem == navBox){
-        barrNav.classList.remove('barr__nav-show')
-    }
+        barrNav.classList.remove('nav__box-show')
+    }else if(elem == linkSubNav[0]){
+        console.log(linkSubNav[0]);
+        navSubBox[1].classList.remove('box__subMenu-1');
+        console.log(navSubBox[1])
+        navSubBox[0].classList.toggle('box__subMenu-0');
+    }else if(elem == linkSubNav[1]){
+        console.log(linkSubNav[1]);
+        navSubBox[0].classList.remove('box__subMenu-0');
+        console.log(navSubBox[0])
+        navSubBox[1].classList.toggle('box__subMenu-1');
+        }
 }
 
 barrBox.addEventListener('click', e=>{
     elem= e.target;
-    console.log(elem)
+    // console.log(elem)
     data(elem);
 })
